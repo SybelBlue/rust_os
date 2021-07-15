0733ae2c521b4e1f2689b526f76d4083704dc27a
L Caraco <lbc.dna@gmail.com>
2021-07-15 15:32:25 -0700

    fixed test runtime errors

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@
 
 #![feature(asm)]
 
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+
 use core::panic::PanicInfo;
 
 #[panic_handler]
@@ -18,4 +21,12 @@ pub extern "C" fn _start() -> ! {
     println!("Hello Scary World{}", "!");
     // panic!("Some panic message");
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }
