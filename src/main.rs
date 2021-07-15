66fd6ba6fe3b423733a799d7df54d2214bd3fab0
L Caraco <lbc.dna@gmail.com>
2021-07-15 15:13:18 -0700

    made a global writer

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,12 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
+mod vga_buffer;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::WRITER.lock().write_string("Hello \n");
+    vga_buffer::WRITER.lock().write_string("again");
 
     loop {}
 }
