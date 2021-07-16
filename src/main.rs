f86cb960415ff6e8b948df414b85830ef50b93e1
L Caraco <lbc.dna@gmail.com>
2021-07-16 08:22:19 -0700

    basic double fault handling

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,16 @@ pub extern "C" fn _start() -> ! {
 
     rust_os::init();
 
-    x86_64::instructions::interrupts::int3();
+    // trigger a page fault
+    unsafe {
+        *(0xdeadbeef as *mut u64) = 42;
+    };
 
     #[cfg(test)]
     test_main();
 
     println!("It didn't crash!");
-    
+
     loop {}
 }
 
