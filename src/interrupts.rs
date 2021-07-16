4e605b472c9753667f5f7512cd5ad9c4ef894df9
L Caraco <lbc.dna@gmail.com>
2021-07-16 10:41:01 -0700

    added PIT interrupts

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -12,6 +12,8 @@ lazy_static! {
                 // unsafe set
                 .set_stack_index(crate::gdt::DOUBLE_FAULT_IST_INDEX);
         }
+        idt[InterruptIndex::Timer.as_usize()]
+            .set_handler_fn(timer_interrupt_handler);
         idt
     };
 }
@@ -30,7 +32,43 @@ extern "x86-interrupt" fn double_fault_handler(
     panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
 }
 
+extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
+    crate::print!(".");
+
+    unsafe {
+        PICS.lock()
+            .notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
+    }
+}
+
 #[test_case]
 fn breakpoint_exception() {
     x86_64::instructions::interrupts::int3();
+}
+
+use pic8259::ChainedPics;
+use spin;
+
+pub const PIC_1_OFFSET: u8 = 32;
+pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
+
+pub static PICS: spin::Mutex<ChainedPics> = 
+    spin::Mutex::new(unsafe {
+        ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET)
+    });
+
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum InterruptIndex {
+    Timer = PIC_1_OFFSET,
+}
+
+impl InterruptIndex {
+    fn as_u8(self) -> u8 {
+        self as u8
+    }
+
+    fn as_usize(self) -> usize {
+        usize::from(self.as_u8())
+    }
 }
