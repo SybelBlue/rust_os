336311ec5d78e1cfd2791c7cfb8907877c35ca4e
L Caraco <lbc.dna@gmail.com>
2021-07-16 17:06:25 -0700

    page fault handler

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,4 +1,4 @@
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::structures::idt::{PageFaultErrorCode, InterruptDescriptorTable, InterruptStackFrame};
 use crate::{print, println};
 
 use lazy_static::lazy_static;
@@ -6,16 +6,23 @@ use lazy_static::lazy_static;
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
+        
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+
+        idt.page_fault.set_handler_fn(page_fault_handler);
+
         unsafe {
             idt.double_fault.set_handler_fn(double_fault_handler)
                 // unsafe set
                 .set_stack_index(crate::gdt::DOUBLE_FAULT_IST_INDEX);
         }
+
         idt[InterruptIndex::Timer.as_usize()]
             .set_handler_fn(timer_interrupt_handler);
+        
         idt[InterruptIndex::Keyboard.as_usize()]
             .set_handler_fn(keyboard_interrupt_handler);
+        
         idt
     };
 }
@@ -76,6 +83,19 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(
         PICS.lock()
             .notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
     }
+}
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: PageFaultErrorCode)
+{
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("Error Code: {:?}", error_code);
+    println!("{:#?}", stack_frame);
+    crate::hlt_loop();
 }
 
 #[test_case]
--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,9 @@ pub extern "C" fn _start() -> ! {
 
     rust_os::init();
     
+    let ptr = 0xdeadbeaf as *mut u32;
+    unsafe { *ptr = 42; }
+    
     #[cfg(test)]
     test_main();
 
